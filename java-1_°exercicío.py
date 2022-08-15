599e33c21e27d1d854c55ce1745a49675319e438
Maria Leonês <109047857+marialeones@users.noreply.github.com>
2022-08-15 14:19:12 -0300

    Add files via upload
    Acesse pelo menos sites de sua preferência e os inspecione com o botão direito do mouse na página web.
    Com o código fonte aberto você deve: 
    
    Registrar nenhum código algum elemento que utilize JavaScript; 
    Marcar alguns elementos do site; 
    Explicar como ele se comporta. Exemplo: entre no site do Google, verifique o botão de pesquisa, o código e explique qual a finalidade do botão.

---
+++ java-1_°exercicío.py
@@ -0,0 +1,12 @@
+""" À linguagem de programação Javascript, permite criar conteúdo que atualiza dinamicamente,
+ao analisar o site do google colab (site utilizado para a codificação de codigos em python), 
+quando um programador (DEV), desenvolve o seu codigo nesse site, e faz teste, logo automaticamente
+ a pagina e atualizada ou seja, necessita de um conteúdo javascript, para ser possivél essa funcionalidade.  
+
+ </script><meta name="referrer" content="origin"/><meta name="viewport" content="width=device-width,
+  initial-scale=1"
+
+  Acima está um codigo disponivel no site do google colab como a utilização do javascript.
+
+
+"""
